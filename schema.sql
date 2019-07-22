ccdda6d2d2640b8f6a7f30de5da294c925cb3903
twardcox <t.ward.cox@gmail.com>
2019-07-22 15:10:05 -0700

    new politicain works

--- schema.sql
+++ schema.sql
@@ -1,25 +1,16 @@
-DROP TABLE IF EXISTS polotitians;
+DROP TABLE IF EXISTS politicians;
 
-CREATE TABLE polititians
+CREATE TABLE politicians
 (
   id SERIAL PRIMARY KEY,
   created_at BIGINT,
   candidate_id VARCHAR(255),
-  -- candidate_id: "P00011569"
   candidate_name VARCHAR(255),
-  -- name: "753, JO",
   party VARCHAR(255),
-  -- party: null,
   size0 INTEGER,
-  -- results[0].total
   size200 BIGINT,
-  -- results[1].total
   size500 BIGINT,
-  -- results[2].total
   size1k BIGINT,
-  -- results[3].total
   size2k BIGINT
-  -- results[4].total
-
 );
 
