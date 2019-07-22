c636ba1d81401b73fdabfd8e27116f8846c6bae1
Nhu Trinh <nqtrinh87@gmail.com>
2019-07-22 15:41:15 -0700

    travis/nhu pairing

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ require('dotenv').config();
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
-app.use(express.static('public'));
-
 // Database Setup
+
+
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
@@ -54,39 +54,18 @@ function handleError(err, res) {
 // }
 
 // Models;
-function Politician(query, res) {
-  this.tableName = 'politicians';
+function Politician(res) {
+  this.tableName = 'politician';
   this.created_at = Date.now();
-  this.candidate_id = res.model.results.candidate_id;
-  this.candidate_name = res.model.results.candidate_name;
-  this.party = res.model.results.party;
-  this.size0 = res.model.results.size0;
-  this.size200 = res.model.results.size0;
-  this.size500 = res.model.results.size0;
-  this.size1k = res.model.results.size0;
-  this.size2k = res.model.results.size0;
-  this.total_disbursements = res.model.results.total_disbursements;
-  this.total_receipts = res.model.results.total_receipts;
+  this.candidate_id = res.candidate.candidate_id;
+  this.candidate_name = res.candidate.name;
+  this.party = res.candidate.party_full;
+  this.size0 = res.financials[0].size;
+  this.size200 = res.financials[1].size;
+  this.size500 = res.financials[2].size;
+  this.size1k = res.financials[3].size;
+  this.size2k = res.financials[4].size;
 }
-
-// id SERIAL PRIMARY KEY,
-//   created_at BIGINT,
-//   candidate_id VARCHAR(255),
-//   -- candidate_id: "P00011569"
-//   candidate_name VARCHAR(255),
-//   -- name: "753, JO",
-//   party VARCHAR(255),
-//   -- party: null,
-//   size0 INTEGER,
-//   -- results[0].total
-//   size200 BIGINT,
-//   -- results[1].total
-//   size500 BIGINT,
-//   -- results[2].total
-//   size1k BIGINT,
-//   -- results[3].total
-//   size2k BIGINT
-//   -- results[4].total
 
 // Politician.lookupPolitician = politician => {
 //   const SQL = `SELECT * FROM locations WHERE candidate_name=$1;`;
@@ -104,32 +83,30 @@ function Politician(query, res) {
 //     .catch(console.error);
 // };
 
-// Politician.prototype = {
-//   save: function() {
-//     const SQL = `INSERT INTO politicians (created_at, candidate_id, candidate_name, total_disbursements, total_receipts) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING id;`;
-//     const values = [this.created_at, this.candidate_id, this.candidate_name, this.total_disbursements, this.total_receipts];
+Politician.prototype = {
+  save: function() {
+    const SQL = `INSERT INTO politician (created_at, candidate_id, candidate_name, party, size0, size200, size500, size1k, size2k) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) ON CONFLICT DO NOTHING RETURNING id;`;
+    const values = [this.created_at, this.candidate_id, this.candidate_name, this.party, this.size0, this.size200, this.size500, this.size1k, this.size2k];
 
-//     return client.query(SQL, values).then(result => {
-//       this.id = result.rows[0].id;
-//       return this;
-//     });
-//   }
-// };
+    return client.query(SQL, values).then(result => {
+      this.id = result.rows[0].id;
+      return this;
+    });
+  }
+};
 
 function getPolitician(request, response) {
   const candidate_name = `Bernie Sanders`;
 
-  let candidateUrl = `https://api.open.fec.gov/v1/candidates/search/?office=P&page=1&sort=name&sort_hide_null=false&per_page=20&sort_null_only=false&api_key=${process.env.FEC_KEY}&name=${candidate_name}&sort_nulls_last=false`;
+  let candidateUrl = `https://api.open.fec.gov/v1/candidates/search/?office=P&page=1&sort=name&sort_hide_null=false&per_page=20&sort_null_only=false&api_key=KmJ4gTUeGUgZZeUrj9Hdr2GIgRZMqQzz4NALZXvD&name=${candidate_name}&sort_nulls_last=false`;
 
   return superagent
     .get(candidateUrl)
     .then(candidateResult => {
-      // const politician = new Politician(this.query, result);
-      // politician.save().then(politician => response.send(politician));
-
       superagent.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_size/by_candidate/?sort_nulls_last=false&page=1&sort_null_only=false&sort_hide_null=false&per_page=20&api_key=${process.env.FEC_KEY}&cycle=2020&candidate_id=${candidateResult.body.results[0].candidate_id}&election_full=false`).then(financialResult => {
-        const result = { candidate: candidateResult.text, financials: financialResult.text };
-        console.log('result: ', result);
+        const result = { candidate: candidateResult.body.results[0], financials: financialResult.body.results };
+        const politician = new Politician(result);
+        politician.save().then(politician => response.send(politician));
       });
     })
     .catch(error => handleError(error));
