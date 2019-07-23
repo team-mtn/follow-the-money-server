37bbd8cbd5077277ace609894fcff363ab9f2663
Nhu Trinh <nqtrinh87@gmail.com>
2019-07-22 17:52:07 -0700

    adding db lookup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-'use strict';
 // Follow-the-money App
+'use strict';
 
 // Application Dependencies
 const express = require('express');
@@ -17,15 +17,18 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 
 // Database Setup
-
-
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
 // API Routes
 app.get('/politician', getPolitician);
-// app.get('/area', getArea);
+app.get('/allpoliticians', getAllPoliticians);
+
+
+app.use('*', (request, response) => {
+  response.status(404).send('you got to the wrong place');
+})
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -35,23 +38,6 @@ function handleError(err, res) {
   console.error(err);
   if (res) res.status(500).send('Sorry, something went wrong');
 }
-
-// Look for the results in the database
-// function lookup(options) {
-//   const SQL = `SELECT * FROM ${options.tableName} WHERE candidate_id=$1;`;
-//   const values = [options.candidate_name];
-
-//   client
-//     .query(SQL, values)
-//     .then(result => {
-//       if (result.rowCount > 0) {
-//         options.cacheHit(result, options.cacheMiss);
-//       } else {
-//         options.cacheMiss();
-//       }
-//     })
-//     .catch(error => handleError(error));
-// }
 
 // Models;
 function Politician(res) {
@@ -67,21 +53,23 @@ function Politician(res) {
   this.size2k = res.financials[4].size;
 }
 
-// Politician.lookupPolitician = politician => {
-//   const SQL = `SELECT * FROM locations WHERE candidate_name=$1;`;
-//   const values = [politician.candidate_name];
+// TODO: test functionality
+Politician.lookup = politician => {
+  const SQL = `SELECT * FROM politician WHERE candidate_name=$1;`;
+  // console.log('---------', politician.candidate_name)
+  const values = [politician.candidate_name];
 
-//   return client
-//     .query(SQL, values)
-//     .then(result => {
-//       if (result.rowCount > 0) {
-//         politician.cacheHit(result);
-//       } else {
-//         politician.cacheMiss();
-//       }
-//     })
-//     .catch(console.error);
-// };
+  return client
+    .query(SQL, values)
+    .then(result => {
+      if (result.rowCount > 0) {
+        politician.cacheHit(result);
+      } else {
+        politician.cacheMiss();
+      }
+    })
+    .catch(console.error);
+};
 
 Politician.prototype = {
   save: function() {
@@ -95,19 +83,40 @@ Politician.prototype = {
   }
 };
 
-function getPolitician(request, response) {
-  const candidate_name = `Bernie Sanders`;
+// TODO: test lookup
+function getPolitician(req, res) {
+  const candidate_name = `SANDERS, BERNARD`;
+  // console.log('req.query.data', req.query.data.toUpperCase())
+  Politician.lookup({
+    tableName: Politician.tableName,
+    query: candidate_name,
+    cacheHit: function (result){
+      res.send(result.rows[0]);
+    },
+    
+    cacheMiss: function () {
+      let candidateUrl = `https://api.open.fec.gov/v1/candidates/search/?office=P&page=1&sort=name&sort_hide_null=false&per_page=20&sort_null_only=false&api_key=KmJ4gTUeGUgZZeUrj9Hdr2GIgRZMqQzz4NALZXvD&name=${candidate_name}&sort_nulls_last=false`;
+    
+      return superagent
+        .get(candidateUrl)
+        .then(candidateResult => {
+          superagent.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_size/by_candidate/?sort_nulls_last=false&page=1&sort_null_only=false&sort_hide_null=false&per_page=20&api_key=${process.env.FEC_KEY}&cycle=2020&candidate_id=${candidateResult.body.results[0].candidate_id}&election_full=false`).then(financialResult => {
+            const result = { candidate: candidateResult.body.results[0], financials: financialResult.body.results };
+            const politician = new Politician(result);
+            politician.save().then(politician => res.send(politician));
+          });
+        })
+        .catch(error => handleError(error));
+    }
+  });
+}
 
-  let candidateUrl = `https://api.open.fec.gov/v1/candidates/search/?office=P&page=1&sort=name&sort_hide_null=false&per_page=20&sort_null_only=false&api_key=KmJ4gTUeGUgZZeUrj9Hdr2GIgRZMqQzz4NALZXvD&name=${candidate_name}&sort_nulls_last=false`;
-
-  return superagent
-    .get(candidateUrl)
-    .then(candidateResult => {
-      superagent.get(`https://api.open.fec.gov/v1/schedules/schedule_a/by_size/by_candidate/?sort_nulls_last=false&page=1&sort_null_only=false&sort_hide_null=false&per_page=20&api_key=${process.env.FEC_KEY}&cycle=2020&candidate_id=${candidateResult.body.results[0].candidate_id}&election_full=false`).then(financialResult => {
-        const result = { candidate: candidateResult.body.results[0], financials: financialResult.body.results };
-        const politician = new Politician(result);
-        politician.save().then(politician => response.send(politician));
-      });
+function getAllPoliticians(req, res){
+  const SQL = `SELECT * FROM politician;`;
+  client.query(SQL)
+    .then(result => {
+      console.log(result)
+      res.send(result.rows)
     })
     .catch(error => handleError(error));
 }
